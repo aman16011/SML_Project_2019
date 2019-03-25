c89628ebbcb8e7b289ece0c92256c298bf182236
AMAN ROY <aman16011@iiitd.ac.in>
2019-03-25 15:49:11 +0530

    Added Two classifiers + Stratified K split

--- code/helper_function.py
+++ code/helper_function.py
@@ -1,6 +1,10 @@
 # all functions go here.
+from sklearn.linear_model import LogisticRegression 
+from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import StratifiedKFold
 import glob
 
+# Data Loader
 def get_data(folder_path):
 
     files = glob(folder_path+"*")
@@ -25,3 +29,32 @@ def get_data(folder_path):
             Y = np.append(Y,Y_temp)
 
     return X,Y
+
+# Logistic Regression Classifier.
+def learn_LR_classifier(X_train,Y_train):
+
+    return LogisticRegression().fit(X_train,Y_train)
+
+# Gaussian Naive Bayes Classifier.
+def learn_naive_bayes_classifier(X_train,Y_train):
+
+    return GaussianNB().fit(X_train,Y_train)
+
+# does k_fold_splitting and is used for CV.
+def stratified_K_fold(split,X,Y):
+    
+    X_train = []
+    Y_train = []
+    X_val = []
+    Y_val = []
+    skf = StratifiedKFold(n_splits=split)
+    for train_index, val_index in skf.split(X,Y):
+        X_train_temp, X_val_temp = X[train_index], X[val_index]
+        Y_train_temp, Y_val_temp = Y[train_index], Y[val_index]
+        X_train.append(X_train_temp)
+        Y_train.append(Y_train_temp)
+        X_val.append(X_val_temp)
+        Y_val.append(Y_val_temp)
+        print(len(X_train_temp),"Training size",len(X_val_temp),"validation size")
+
+    return X_train,Y_train,X_val,Y_val
